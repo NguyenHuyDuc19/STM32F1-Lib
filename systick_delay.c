6cc900dadf205f215d39170f05610f1e4c2c1066
NguyenHuyDuc <ducnguyenhuy2000@gmail.com>
2022-03-07 22:59:40 +0700

    Systick Delay Library

--- systick_delay.c
+++ systick_delay.c
@@ -4,24 +4,4 @@ uint32_t tick;
 
 
 
-void SysTick_Handler(void)
-{
-	tick++;
-}
-
-void delayMs(int ms)
-{
-	SysTick_Config(SystemCoreClock /1000); //Configure SysTick to generate an interrupt every millisecond
-	tick = 0;
-	while(tick < ms);
-}
-
-
-void delayUs(int us)
-{
-	SysTick_Config(SystemCoreClock /1000000); //Configure SysTick to generate an interrupt every microsecond
-	tick = 0;
-	while(tick < us);
-}
-
 
